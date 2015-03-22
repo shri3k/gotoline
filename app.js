97ef527a36bd319723140fd12dcb1ea9cc37250f
yojan shrestha <sickgrin@gmail.com>
2015-03-22 01:46:34 -0500

    no frames only dreams

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ var app = module.exports = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('view engine', 'html');
+app.engine('html', require('hbs').__express);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
--- routes/page.js
+++ routes/page.js
@@ -1,3 +1,5 @@
+var http = require('http');
+var https = require('https');
 var express = require('express');
 var router = express.Router();
 var pageModel = require('../models/page');
@@ -11,10 +13,38 @@ router.get('/:id', function(req, response, next) {
     if (err) {
       throw new Error(err);
     }
-    response.render('page', {
-      id: req.params.id,
-      src: res
-    });
+    try {
+      http.get(res.toString(), function(dat) {
+        var serve = '';
+        dat.on('data', function(res) {
+          serve += res.toString();
+        });
+        dat.on('end', function() {
+          var slicedUp = serve.split(/(<\/body>)/);
+          slicedUp.splice(2, 0, "<script>window.onload = function(){ window.scrollTo(0,500);}</script>");
+          console.log(slicedUp);
+          response.status(200).send(slicedUp.join());
+        });
+      });
+    } catch (e) {
+      console.log("error on", e);
+      https.get(res.toString(), function(dat) {
+        var serve = '';
+        dat.on('data', function(res) {
+          serve += res.toString();
+        });
+        dat.on('end', function() {
+          var slicedUp = serve.split(/(<\/body>)/);
+          slicedUp.splice(2, 0, "<script>window.onload = function(){ window.scrollTo(0,500);}</script>");
+          console.log(slicedUp);
+          response.status(200).send(slicedUp.join());
+        });
+      });
+    }
+    // response.render('page', {
+    //   id: req.params.id,
+    //   src: res
+    // });
   });
 });
 
