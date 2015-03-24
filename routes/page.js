78a42e41c74f3da9da4f46198b4925f931632c64
yojan shrestha <sickgrin@gmail.com>
2015-03-24 11:42:01 -0500

    refactor

--- routes/page.js
+++ routes/page.js
@@ -4,6 +4,19 @@ var express = require('express');
 var router = express.Router();
 var pageModel = require('../models/page');
 
+function injectScript(dat) {
+  var serve = '';
+  dat.on('data', function(res) {
+    serve += res.toString();
+  });
+  dat.on('end', function() {
+    var slicedUp = serve.split(/(<\/body>)/);
+    slicedUp.splice(2, 0, "<script>window.onload = function(){ window.scrollTo(0,500);}</script>");
+    console.log(slicedUp);
+    response.status(200).send(slicedUp.join());
+  });
+}
+
 router.get('/', function(req, res, next) {
   res.render('page', pageModel.getPage());
 });
@@ -14,37 +27,11 @@ router.get('/:id', function(req, response, next) {
       throw new Error(err);
     }
     try {
-      http.get(res.toString(), function(dat) {
-        var serve = '';
-        dat.on('data', function(res) {
-          serve += res.toString();
-        });
-        dat.on('end', function() {
-          var slicedUp = serve.split(/(<\/body>)/);
-          slicedUp.splice(2, 0, "<script>window.onload = function(){ window.scrollTo(0,500);}</script>");
-          console.log(slicedUp);
-          response.status(200).send(slicedUp.join());
-        });
-      });
+      http.get(res.toString(), injectScript);
     } catch (e) {
-      console.log("error on", e);
-      https.get(res.toString(), function(dat) {
-        var serve = '';
-        dat.on('data', function(res) {
-          serve += res.toString();
-        });
-        dat.on('end', function() {
-          var slicedUp = serve.split(/(<\/body>)/);
-          slicedUp.splice(2, 0, "<script>window.onload = function(){ window.scrollTo(0,500);}</script>");
-          console.log(slicedUp);
-          response.status(200).send(slicedUp.join());
-        });
-      });
+      console.log("trying https", e);
+      https.get(res.toString(), injectScript);
     }
-    // response.render('page', {
-    //   id: req.params.id,
-    //   src: res
-    // });
   });
 });
 
