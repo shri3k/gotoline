84a3cc7114abe57bf8299210d69374f4d699448e
yojan shrestha <sickgrin@gmail.com>
2015-04-02 02:35:33 -0500

    Added the pattern, fixed the url pass
    - [x] scrollify now has es6 string interpolation
    - [x] positionify now returns % of the page
    - [x] url can be passed easily to positionify

--- routes/page.js
+++ routes/page.js
@@ -3,29 +3,31 @@ var https = require('https');
 var express = require('express');
 var router = express.Router();
 var pageModel = require('../models/page');
-var scrollify = require('../lib/scroll');
+var positionify = require('../lib/scroll');
+var scrollify = require('../scripts/scroll');
 
 router.get('/', function(req, res, next) {
   res.render('page', pageModel.getPage());
 });
 
 router.get('/:id', function(req, response, next) {
+  var url = res.toString();
   pageModel.getPage(req.params.id, function(err, res) {
     if (err) {
       throw new Error(err);
     }
     try {
-      http.get(res.toString(), injectScript(response));
+      http.get(url, injectScript(response, url));
     } catch (e) {
       console.log("trying https..");
-      https.get(res.toString(), injectScript(response));
+      https.get(url, injectScript(response, url));
     }
   });
 });
 
 module.exports = router;
 
-function injectScript(resp) {
+function injectScript(resp, url) {
   return function(dat) {
     var serve = '';
     dat.on('data', function(res) {
@@ -33,7 +35,7 @@ function injectScript(resp) {
     });
     dat.on('end', function() {
       var slicedUp = serve.split(/(<\/body>)/);
-      slicedUp.splice(2, 0, "<script>window.onload = function(){ window.scrollTo(0,500);}</script>");
+      slicedUp.splice(2, 0, scrollify(0, positionify.getScrollTo(url)));
       // console.log(slicedUp);
       resp.status(200).send(slicedUp.join());
     });
