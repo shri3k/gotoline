d085eb41ab8947dc463800c556aa040b245144d8
yojan shrestha <sickgrin@gmail.com>
2015-04-17 18:26:25 -0500

    Added CORS verification

--- public/javascripts/bookmarklet.js
+++ public/javascripts/bookmarklet.js
@@ -1,9 +1,15 @@
 (function(){
     var xhr = new XMLHttpRequest();
+    if(!('withCredentials' in xhr)){
+        alert('Browser dont support rando');
+    }
     xhr.open('POST','http://localhost:3000/', true);
     xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
     xhr.onload = function(res){
       console.log(res);
+    };
+    xhr.onerror = function(){
+      console.log('something happended!! oh no!!');
     };
     xhr.send('url='+window.location.href+'#400');
 }());
