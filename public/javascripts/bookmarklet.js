a0ac7e6e4b4b63757156d76d1dadb3e4ba62a883
yojan shrestha <sickgrin@gmail.com>
2015-04-18 00:49:39 -0500

    Added HUD display

--- public/javascripts/bookmarklet.js
+++ public/javascripts/bookmarklet.js
@@ -1,15 +1,27 @@
-(function(){
-    var xhr = new XMLHttpRequest();
-    if(!('withCredentials' in xhr)){
-        alert('Browser dont support rando');
-    }
-    xhr.open('POST','http://localhost:3000/', true);
-    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    xhr.onload = function(res){
-      console.log(res);
-    };
-    xhr.onerror = function(){
-      console.log('something happended!! oh no!!');
-    };
-    xhr.send('url='+window.location.href+'#400');
+(function() {
+  function display(msg) {
+    var element = document.createElement('div');
+    element.textContent = msg;
+    element.style.position = "fixed";
+    element.style.bottom = '1px';
+    element.style.textAlign = 'center';
+    element.style.width = '100%';
+    element.style.backgroundColor= "LightYellow"
+    document.body.appendChild(element);
+  }
+  var url = 'http://localhost:3000';
+  var method = 'POST';
+  var xhr = new XMLHttpRequest();
+  if (!('withCredentials' in xhr)) {
+    alert('Browser dont support rando');
+  }
+  xhr.open(method, url);
+  xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+  xhr.onload = function(res) {
+    display(this.responseText);
+  };
+  xhr.onerror = function() {
+    console.log('something happended!! oh no!!');
+  };
+  xhr.send('url=' + window.location.href + '#400');
 }());
